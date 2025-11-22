4d228b508afc749a8f8a97b0dfde8bb35cb91fa1
Emjeeeeiy <mangamj471@gmail.com>
2025-11-23 01:38:56 +0800

    try

--- src/pages/user-pages/billing.vue
+++ src/pages/user-pages/billing.vue
@@ -45,7 +45,7 @@
                 <input
                   v-model="searchQuery"
                   type="text"
-                  placeholder="Search..."
+                  placeholder="Search ..."
                   class="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 text-sm shadow-sm"
                 />
               </div>
